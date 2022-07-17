ff9e7618c4b67ac69a5c6ac14688acaa5294c53e
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-07-17 20:46:36 -0300

    Create a serivce class to handle users

---
+++ app/Providers/UserServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            \App\Contracts\UserService::class,
+            \App\Services\UserService::class,
+        );
+    }
+}
