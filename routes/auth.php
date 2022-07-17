d3ef664e1b870f6b10794ad1075d311e91c62f09
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-07-17 20:49:27 -0300

    Create sign up route

--- routes/auth.php
+++ routes/auth.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\SignUpController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', LoginController::class)
     ->name('login');
+
+Route::post('/signup', SignUpController::class)
+    ->name('signup');
