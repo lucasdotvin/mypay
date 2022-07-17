f5af2dbf26d29cb1e9f335bfcd64d4eeadc93341
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-07-17 19:35:31 -0300

    Create a trait to ensure test suites have mandatory data filled in the database

---
+++ tests/Traits/FillDatabaseWithMandatoryData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Traits;
+
+use Database\Seeders\MandatoryDataSeeder;
+use Illuminate\Support\Facades\Schema;
+
+trait FillDatabaseWithMandatoryData
+{
+    /**
+     * Setup up the database with mandatory data.
+     *
+     * @return void
+     * @throws IncompleteTestError if the database is not ready.
+     */
+    public function setUpFillDatabaseWithMandatoryData()
+    {
+        if (! Schema::hasTable('roles')) {
+            static::markTestIncomplete('It was not possible to fill the database with mandatory data. Check the database-related traits of the current test suite.');
+        }
+
+        $this->seed(MandatoryDataSeeder::class);
+    }
+}
