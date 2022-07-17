692bf9b2af62700cba51f0b2b13d944dc1623b19
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-07-17 19:35:12 -0300

    Create a database seeder to the roles

---
+++ database/seeders/MandatoryDataSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MandatoryDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::transaction(function () {
+            $this->call(RoleSeeder::class);
+        });
+    }
+}
