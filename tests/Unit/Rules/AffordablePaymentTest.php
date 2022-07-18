8729a23b8982aed76611e8032ceaba906cdd812b
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-07-18 08:14:00 -0300

    Create a test suite over a rule to validate payment amount

---
+++ tests/Unit/Rules/AffordablePaymentTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit\Rules;
+
+use App\Contracts\Payments\PaymentService;
+use App\Rules\AffordablePayment;
+use Illuminate\Support\Facades\Validator;
+use Mockery\MockInterface;
+use Tests\TestCase;
+
+class AffordablePaymentTest extends TestCase
+{
+    /** @test */
+    public function it_returns_true_for_affordable_payments()
+    {
+        $this->mock(PaymentService::class, function (MockInterface $mock) {
+            $mock->shouldReceive('canAfford')
+                ->once()
+                ->andReturn(true);
+        });
+
+        $rule = new AffordablePayment;
+        $passes = $rule->passes('amount', 99);
+
+        $this->assertTrue($passes);
+    }
+
+    /** @test */
+    public function it_returns_false_for_not_affordable_payments()
+    {
+        $this->mock(PaymentService::class, function (MockInterface $mock) {
+            $mock->shouldReceive('canAfford')
+                ->once()
+                ->andReturn(false);
+        });
+
+        $rule = new AffordablePayment;
+        $passes = $rule->passes('amount', 101);
+
+        $this->assertFalse($passes);
+    }
+
+    /** @test */
+    public function it_returns_the_expected_error_message()
+    {
+        $rule = new AffordablePayment;
+
+        $message = $rule->message();
+
+        $this->assertEquals('validation.affordable_payment', $message);
+    }
+}
