d56135ebda4d073ef17fe33a78249eb890f69e50
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-07-18 08:55:48 -0300

    Move render logic to exception class

--- app/Exceptions/Payments/NonSufficientFunds.php
+++ app/Exceptions/Payments/NonSufficientFunds.php
@@ -6,4 +6,17 @@ use Exception;
 
 class NonSufficientFunds extends Exception
 {
+    public function __construct() {
+        $this->message = trans('exceptions.non_sufficient_funds');
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function render()
+    {
+        return response()->json(['message' => $this->message], 400);
+    }
 }
