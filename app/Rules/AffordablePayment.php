6b84c2885a8c7b0e290571050dd992e7834adb53
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-07-18 08:14:15 -0300

    Create a rule class to validate payments' amount

---
+++ app/Rules/AffordablePayment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Rules;
+
+use App\Contracts\Payments\PaymentService;
+use Illuminate\Contracts\Validation\Rule;
+
+class AffordablePayment implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return app(PaymentService::class)->canAfford($value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'validation.affordable_payment';
+    }
+}
