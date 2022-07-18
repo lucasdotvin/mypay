46ffab07a6c6dd0a4e216a8467fa723f21da58df
Lucas Vinicius <lucasvinicius211@yahoo.com>
2022-07-18 09:04:27 -0300

    Create test cases over payment creation

--- tests/Feature/Http/PaymentControllerTest.php
+++ tests/Feature/Http/PaymentControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http;
 
+use App\Contracts\Payments\Authorization\AuthorizationService;
 use App\Models\Payment;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -62,5 +63,234 @@ class PaymentControllerTest extends TestCase
                             ->has('payer')
                     )
             );
+    }
+
+    /** @test */
+    public function it_requires_an_authenticated_user_to_create_a_payment()
+    {
+        $response = $this->postJson(route('payments.store'));
+
+        $response->assertUnauthorized();
+    }
+
+    /** @test */
+    public function it_requires_a_payee_id_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'));
+
+        $response->assertJsonValidationErrors('payee_id');
+    }
+
+    /** @test */
+    public function it_requires_a_valid_payee_id_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'), [
+            'payee_id' => -1,
+        ]);
+
+        $response->assertJsonValidationErrors('payee_id');
+    }
+
+    /** @test */
+    public function it_requires_a_payee_id_different_from_the_current_user_id_to_create_a_payment()
+    {
+        Sanctum::actingAs($user = User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'), [
+            'payee_id' => $user->id,
+        ]);
+
+        $response->assertJsonValidationErrors('payee_id');
+    }
+
+    /** @test */
+    public function it_requires_an_amount_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'));
+
+        $response->assertJsonValidationErrors('amount');
+    }
+
+    /** @test */
+    public function it_requires_a_positive_amount_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'), [
+            'amount' => -1,
+        ]);
+
+        $response->assertJsonValidationErrors('amount');
+    }
+
+    /** @test */
+    public function it_requires_a_not_null_amount_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'), [
+            'amount' => 0,
+        ]);
+
+        $response->assertJsonValidationErrors('amount');
+    }
+
+    /** @test */
+    public function it_requires_an_affordable_amount_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne([
+            'balance' => 100,
+        ]));
+
+        $response = $this->postJson(route('payments.store'), [
+            'amount' => 101,
+        ]);
+
+        $response->assertJsonValidationErrors('amount');
+    }
+
+    /** @test */
+    public function it_requires_an_integer_amount_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne([
+            'balance' => 100,
+        ]));
+
+        $response = $this->postJson(route('payments.store'), [
+            'amount' => 99.99,
+        ]);
+
+        $response->assertJsonValidationErrors('amount');
+    }
+
+    /** @test */
+    public function it_requires_message_to_be_present_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'));
+
+        $response->assertJsonValidationErrors('message');
+    }
+
+    /** @test */
+    public function it_requires_message_to_be_a_string_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'), [
+            'message' => 1,
+        ]);
+
+        $response->assertJsonValidationErrors('message');
+    }
+
+    /** @test */
+    public function it_requires_message_to_be_a_maximum_of_255_characters_to_create_a_payment()
+    {
+        Sanctum::actingAs(User::factory()->createOne());
+
+        $response = $this->postJson(route('payments.store'), [
+            'message' => str_repeat('#', 256),
+        ]);
+
+        $response->assertJsonValidationErrors('message');
+    }
+
+    /** @test */
+    public function it_creates_a_payment()
+    {
+        $payee = User::factory()->createOne();
+
+        Sanctum::actingAs($user = User::factory()->createOne([
+            'balance' => 100,
+        ]));
+
+        $response = $this->postJson(route('payments.store'), [
+            'payee_id' => $payee->id,
+            'amount' => 100,
+            'message' => 'Payment for something',
+        ]);
+
+        $this->assertDatabaseHas('payments', [
+            'id' => $response->json('id'),
+            'amount' => 100,
+            'message' => 'Payment for something',
+            'payer_id' => $user->id,
+            'payee_id' => $payee->id,
+        ]);
+    }
+
+    /** @test */
+    public function it_updates_payee_balance()
+    {
+        $payee = User::factory()->createOne();
+
+        Sanctum::actingAs(User::factory()->createOne([
+            'balance' => 100,
+        ]));
+
+        $this->postJson(route('payments.store'), [
+            'payee_id' => $payee->id,
+            'amount' => 100,
+            'message' => 'Payment for something',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $payee->id,
+            'balance' => 100,
+        ]);
+    }
+
+    /** @test */
+    public function it_updates_payer_balance()
+    {
+        $payee = User::factory()->createOne();
+
+        Sanctum::actingAs($user = User::factory()->createOne([
+            'balance' => 100,
+        ]));
+
+        $this->postJson(route('payments.store'), [
+            'payee_id' => $payee->id,
+            'amount' => 100,
+            'message' => 'Payment for something',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'balance' => 0,
+        ]);
+    }
+
+    /** @test */
+    public function it_returns_error_if_the_payment_was_not_authorized()
+    {
+        $payee = User::factory()->createOne();
+
+        Sanctum::actingAs(User::factory()->createOne([
+            'balance' => 100,
+        ]));
+
+        $this->mock(AuthorizationService::class, function ($mock) {
+            $mock->shouldReceive('authorize')->andReturn(false);
+        });
+
+        $response = $this->postJson(route('payments.store'), [
+            'payee_id' => $payee->id,
+            'amount' => 100,
+            'message' => 'Payment for something',
+        ]);
+
+        $response->assertStatus(500)
+            ->assertJson([
+                'message' => trans('exceptions.denied_payment'),
+            ]);
     }
 }
